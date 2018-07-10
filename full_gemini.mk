b97371d497a7063f99ce3151cb156a95a3ef6443
Bruno Martins <bgcngm@gmail.com>
2018-07-10 18:48:26 +0100

    gemini: Update build fingerprint from Oreo-based MIUI 9 Global Stable
     * Newly released, just few days after the last build fingerprint update
    
    Change-Id: I34df26424ac952ddaa891375df69113adf60f108

--- full_gemini.mk
+++ full_gemini.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -28,4 +28,3 @@ PRODUCT_DEVICE := gemini
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 5
 PRODUCT_MANUFACTURER := Xiaomi
-
