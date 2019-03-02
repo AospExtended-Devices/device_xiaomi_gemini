951cf5e5f92e8171c71a3c58b2a5db35311000e8
sirmordred <oguzhan97yigit@gmail.com>
2019-03-03 00:33:07 +0300

    gemini: configpanel: Port to AOSP
    Signed-off-by: iKeramat <iKeramat80@gmail.com>

--- configpanel/Android.mk
+++ configpanel/Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_CERTIFICATE := platform
-LOCAL_PRIVILEGED_MODULE := true
+LOCAL_VENDOR_MODULE := true
 LOCAL_PACKAGE_NAME := ConfigPanel
 LOCAL_PRIVATE_PLATFORM_APIS := true
 
@@ -12,9 +12,6 @@ LOCAL_USE_AAPT2 := true
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx.core_core \
     androidx.preference_preference
-
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    org.lineageos.platform.internal
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
